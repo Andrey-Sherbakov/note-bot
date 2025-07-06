08ac621ebe34ae0164d20d5461e1585786b52a60
Andrey-Sherbakov <63076931+Andrey-Sherbakov@users.noreply.github.com>
2025-07-06 16:08:30 +0300

    Update: main.py

--- src/main.py
+++ src/main.py
@@ -13,7 +13,15 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def handle_start(message: Message) -> None:
-    await message.answer(f"This simple note taking bot.\nWelcome, {message.from_user.full_name}!")
+    await message.answer(
+        f"Welcome, {message.from_user.full_name}!\nThis is simple note taking bot.\n"
+        f"Use /help to see available commands."
+    )
+
+
+@dp.message(Command("help"))
+async def handle_help(message: Message) -> None:
+    await message.answer("Available commands:\n - /start\n - /help")
 
 
 async def main() -> None:
